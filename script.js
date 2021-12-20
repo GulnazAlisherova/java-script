5b17372919efba40f6e3003a3f337f7ea630ec43
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-20 15:11:47 +0600

    added new names

--- script.js
+++ script.js
@@ -3,15 +3,21 @@
 // console.log("Hello world");
 
 //declaration
-var firstName;
+var firstName, firstN;
 
 //initialite 
 var lastName = "Volkov", middleName = "vasya", tall = "10";
 
 // assignment
-firstN = "Galya"
-firstName = "Vasya";
-firstName = "Petya";
-console.log(firstN);
+just = "Hello"
+firstName = "Galya"
+firstName1 = "Vasya";
+firstName2 = "Petya";
+firstName3 = "Bye<3";
+
+console.log(just)
 console.log(firstName);
+console.log(firstName1);
+console.log(firstName2);
+console.log(firstName3);
 
